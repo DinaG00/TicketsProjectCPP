5cec37f67a525386e75bc9122d5c3f857de32a97
Mona Lisa <gavrilescudina22@stud.ase.ro>
2023-11-21 19:22:10 +0200

    decided on the 3 classes

--- ProjectOOP/ProjectOOP/Source.cpp
+++ ProjectOOP/ProjectOOP/Source.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+//---------------------------------------------------------------------------------------------------------------------
+//reading the characteristics of the event location 
+//(eg: maximum number of seats, number of rows , zones, number/coding of seats per row, etc.)
+class Location
+{
+
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+//reading the characteristics of the event
+//(eg: date, time, name, etc.)
+class event
+{
+
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+//nominal tickets according to the desired characteristics
+//(eg: VIP, lawn, tribune, boxes, etc.)
+class seatType
+{
+
+};
+
 
 int main()
 {
-	cout << endl << "hello world";
+	
 }
