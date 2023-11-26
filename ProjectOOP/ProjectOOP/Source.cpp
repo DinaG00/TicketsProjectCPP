c7fb58a83ee311f1544cd92d7204c53ad5341192
Mona Lisa <gavrilescudina22@stud.ase.ro>
2023-11-26 18:34:07 +0200

    repared << for event

--- ProjectOOP/ProjectOOP/Source.cpp
+++ ProjectOOP/ProjectOOP/Source.cpp
@@ -8,10 +8,15 @@ using namespace std;
 //location(eg: maximum number of seats, number of rows, zones, number / coding of seats per row, etc.)
 class Location
 {
-	int totalSeatsNo = 0;
-	int totalRowsNo = 0;
-	int totalZonesNo = 0;
+	string name = "";
+	int totalSeatsNo = 1;
+	int totalRowsNo = 1;
+	int totalZonesNo = 1;
+	string* zoneNames = new string[totalZonesNo];
+	int* zoneFirstRow = new int[totalZonesNo];
+	int* zoneLastRow = new int[totalZonesNo];
 
+	//overload =, !=	
 
 };
 
@@ -72,22 +77,23 @@ public:
 			case Months::November:
 				MAX_DAY = 30;
 			case Months::February:
-			if (this->year % 400 == 0) 
+			{if (this->year % 400 == 0)
 				MAX_DAY = 29;
-			else 
-				{
+			else
+			{
 				if (this->year % 100 == 0)
 					MAX_DAY = 28;
-				else 
-					{
+				else
+				{
 					if (this->year % 4 == 0)
 						MAX_DAY = 29;
 					else
 						MAX_DAY = 28;
-					}
 				}
+			}
 			default:
 				MAX_DAY = 31;
+			}
 			}
 			if (this->day > Date::MAX_DAY)
 				throw exception("wrong day");
@@ -130,9 +136,9 @@ public:
 
 	void setYear(int value)
 	{
-		/*if (year < 2000 || year>2150)
+		if (value < 2000 || value>2150)
 			throw exception("Wrong year");
-		else*/
+		else
 			this->year = value;
 	}
 
@@ -152,32 +158,42 @@ public:
 		this->month = newDate.month;
 		this->year = newDate.year;
 	}
+
+	bool operator>(Date newDate)
+	{
+		if ((newDate.year > this->year) || (newDate.year == this->year && newDate.month>this->month) || (newDate.year == this->year && newDate.month == this->month && newDate.day>this->day))
+			return 0;
+		else
+			return 1;
+			
+	}
 };
 int Date::MAX_DAY = 31;
 
+//overload cout for Date
 ostream& operator<<(ostream& console, Date& d)
 {
 	console <<endl<<"Date : "<< d.getDay() << "/" << d.getMonth() << "/" << d.getYear();
 	return console;
 }
-
-//void operator>>(istream& console, Date&d)
-//{
-//	cout << endl << "---------Event Date---------";
-//	int day;
-//	cout << endl << "Event day:";
-//	console >> day;
-//	d.setDay(day);
-//	Months m;
-//	cout << endl << "Event month:";
-//	console >> m;
-//	d.setMonth(m);
-//	int y;
-//	cout << endl << "Event year:";
-//	console >> y;
-//	d.setYear(y);
-//	
-//}
+//overload cin for Date
+void operator>>(istream& console, Date&d)
+{
+	cout << endl << "---------Event Date---------";
+	int day;
+	cout << endl << "Event day:";
+	console >> day;
+	d.setDay(day);
+	int m;
+	cout << endl << "Event month:";
+	console >> m;
+	d.setMonth(static_cast<Months>(m));
+	int y;
+	cout << endl << "Event year:";
+	console >> y;
+	d.setYear(y);
+	
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Time
@@ -284,7 +300,17 @@ public:
 	{
 		return this->details;
 	}
-private:
+
+	Date getDate()
+	{
+		return this->eventDate;
+	}
+
+	Time getTime()
+	{
+		return this->eventTime;
+	}
+
 	//setters:
 
 	void setName(string newName) {
@@ -299,6 +325,16 @@ private:
 			throw exception("Details too short");
 		}
 		this->details = newDetails;
+	}
+
+	void setDate(Date newDate)
+	{
+		this->eventDate = newDate;
+	}
+
+	void setTime(Time newTime)
+	{
+		this->eventTime = newTime;
 	}
 
 	//methods:
@@ -321,6 +357,8 @@ ostream& operator<<(ostream& console, Event& e)
 	console << endl << "-------------EVENT--------------------";
 	console << endl << "Event name is : " << e.getName();
 	console << endl << "Event details : " << e.getDetails();
+	console << endl << "Event date : " << e.getDate().getDay() << "/" << e.getDate().getMonth() << "/" << e.getDate().getYear();
+	console << endl << "Event time : "<<e.getTime().getHour()<<":"<<e.getTime().getMinute();
 	return console;
 }
 
@@ -329,11 +367,19 @@ ostream& operator<<(ostream& console, Event& e)
 //(eg: VIP, lawn, tribune, boxes, etc.)
 
 
-//row number, seat number, price, event, const id
-//class Ticket
-//{
-//	
-//};
+/*row number, seat number, price, event, const id
+class Ticket
+{
+	const id=1;
+	string zoneName;
+	int rowNo;
+	int seatNo;
+	double price;
+	string eventName;
+
+
+};
+*/
 
 
 
@@ -353,4 +399,16 @@ int main()
 	/*Time t1;
 	cin >> t1;
 	cout << t1;*/
+
+	/*Date date1;
+	cin >> date1;
+	cout << date1;
+	Date date2;
+	cin >> date2;
+	cout << date2;*/
+	/*Date date1(15, February, 2024);
+	Time time1(16, 15);
+	Event event1("Concert Delia", "concert blablabla", date1, time1);
+	cout << event1;*/
+	
 }
