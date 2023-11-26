d42820c2ab168bc4039bfafcb11e458589aeec83
Mona Lisa <gavrilescudina22@stud.ase.ro>
2023-11-26 13:22:14 +0200

    class date, class time, class event completed

--- ProjectOOP/ProjectOOP/Source.cpp
+++ ProjectOOP/ProjectOOP/Source.cpp
@@ -1,23 +1,24 @@
 ﻿#include <iostream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
-//---------------------------------------------------------------------------------------------------------------------
-//reading the characteristics of the event location 
-//(eg: maximum number of seats, number of rows , zones, number/coding of seats per row, etc.)
-//class Location
-//{
-//	int totalSeatsNo = 0;
-//	int totalRowsNo = 0;
-//	int totalZonesNo = 0;
-//
-//
-//};
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+//location(eg: maximum number of seats, number of rows, zones, number / coding of seats per row, etc.)
+class Location
+{
+	int totalSeatsNo = 0;
+	int totalRowsNo = 0;
+	int totalZonesNo = 0;
 
 
+};
 
-enum Months {January = 1,February,March,April,May,June,July};
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+enum Months {January = 1,February,March,April,May,June,July,August,September,October,November,December};
+
 class Date
 {
 	int day;
@@ -28,13 +29,19 @@ public:
 
 	static int MAX_DAY;
 
+	//default constructor
+	Date()
+	{
+
+	}
+	//constructor
 	Date(int day, Months month, int year)
 	{
 		this->setDay(day);
 		this->setMonth(month);
 		this->setYear(year);
 	}
-private:
+
 	//getters:
 	int getDay()
 	{
@@ -50,7 +57,6 @@ private:
 	{
 		return this->year;
 	}
-
 	//setters:
 	void setMonth(Months value)
 	{
@@ -60,12 +66,12 @@ private:
 		{
 			this->month = value;
 			switch (value) {
-			case 4:
-			case 6:
-			case 9:
-			case 11:
+			case Months::April:
+			case Months::June:
+			case Months::September:
+			case Months::November:
 				MAX_DAY = 30;
-			case 2:
+			case Months::February:
 			if (this->year % 400 == 0) 
 				MAX_DAY = 29;
 			else 
@@ -92,16 +98,30 @@ private:
 	void setDay(int value)
 	{
 		switch (this->month) {
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			Date::MAX_DAY = 30;
-		case 2:
-			Date::MAX_DAY = 28;
+		case Months::April:
+		case Months::June:
+		case Months::September:
+		case Months::November:
+			MAX_DAY = 30;
+		case Months::February:
+			if (this->year % 400 == 0)
+				MAX_DAY = 29;
+			else
+			{
+				if (this->year % 100 == 0)
+					MAX_DAY = 28;
+				else
+				{
+					if (this->year % 4 == 0)
+						MAX_DAY = 29;
+					else
+						MAX_DAY = 28;
+				}
+			}
 		default:
-			Date::MAX_DAY = 31;
+			MAX_DAY = 31;
 		}
+
 		if (value < 1 || value > MAX_DAY)
 			throw exception("wrong day");
 		else
@@ -110,27 +130,75 @@ private:
 
 	void setYear(int value)
 	{
-		if (year < 2000 || year>2150)
+		/*if (year < 2000 || year>2150)
 			throw exception("Wrong year");
-		else
+		else*/
 			this->year = value;
+	}
+
+public:
+	//methods
+
+	void printDate()
+	{
+		cout << this->day << "/" << this->month << "/" << this->year ;
+	}
+
+	//overloading:
+
+	void operator=(Date newDate)
+	{
+		this->day = newDate.day;
+		this->month = newDate.month;
+		this->year = newDate.year;
 	}
 };
 int Date::MAX_DAY = 31;
 
-//----------------------------------------------------------------------------------------------------------------------------------------------------------
+ostream& operator<<(ostream& console, Date& d)
+{
+	console <<endl<<"Date : "<< d.getDay() << "/" << d.getMonth() << "/" << d.getYear();
+	return console;
+}
+
+//void operator>>(istream& console, Date&d)
+//{
+//	cout << endl << "---------Event Date---------";
+//	int day;
+//	cout << endl << "Event day:";
+//	console >> day;
+//	d.setDay(day);
+//	Months m;
+//	cout << endl << "Event month:";
+//	console >> m;
+//	d.setMonth(m);
+//	int y;
+//	cout << endl << "Event year:";
+//	console >> y;
+//	d.setYear(y);
+//	
+//}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 class Time
 {
 	int hour;
 	int minute;
 
 public:
+
+	//default constructot
+	Time()
+	{
+
+	}
+	//constructor
 	Time(int hour, int minute)
 	{
 		this->setHour(hour);
 		this->setMinute(minute);
 	}
-private:
+
 	//getters:
 	int getHour()
 	{
@@ -141,6 +209,7 @@ private:
 	{
 		return this->minute;
 	}
+
 
 	//setters:
 	void setHour(int value)
@@ -157,40 +226,113 @@ private:
 		else
 			this->minute = value;
 	}
+
+
+	void printTime()
+	{
+		cout << this->hour << ":" << this->minute;
+	}
 };
+
+ostream& operator<<(ostream& console, Time& t)
+{
+	console <<endl<<"time : " << t.getHour() << ":" << t.getMinute();
+	return console;
+}
+
+void operator>>(istream& console, Time& t) {
+	cout << endl << "------------Event Time-----------";
+	cout << endl << "Event hour is : ";
+	int h;
+	console >> h;
+	t.setHour(h);
+	cout << endl << "Event minute is : ";
+	int m;
+	console >> m; 
+	t.setMinute(m);
+}
+
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //reading the characteristics of the event
 //(eg: date, time, name, etc.)
+
 class Event
 {
-	string name;
-	string details;
+	string name = "";
+	string details = "";
 	Date eventDate;
-	int hour;							
-	int minute;
+	Time eventTime;
 
+public:
+	Event(string name, string details, Date date, Time time)
+	{
+		this->setName(name);
+		this->setDetails(details);
+		this->eventDate = date;
+		this->eventTime = time;
+	}
 
+	//getters:
+
+	string getName()
+	{
+		return this->name;
+	}
+
+	string getDetails()
+	{
+		return this->details;
+	}
+private:
+	//setters:
+
+	void setName(string newName) {
+		if (newName.size() <= 3 || newName.size()>=100) {
+			throw exception("Name too short");
+		}
+		this->name = newName;
+	}
+
+	void setDetails(string newDetails) {
+		if (newDetails.size() <= 3 || newDetails.size() >= 100) {
+			throw exception("Details too short");
+		}
+		this->details = newDetails;
+	}
+
+	//methods:
+public:
+	void printEvent()
+	{
+		cout << endl << "-------------EVENT--------------------";
+		cout << endl << "Event name is : " << this->name;
+		cout << endl << "Event details : " << this->details;
+		cout << endl << "Event date : ";
+		this->eventDate.printDate();
+		cout << endl << "Event time : ";
+		this->eventTime.printTime();
+	}
 
 };
 
-//----------------------------------------------------------------------------------------------------------------------
+ostream& operator<<(ostream& console, Event& e)
+{
+	console << endl << "-------------EVENT--------------------";
+	console << endl << "Event name is : " << e.getName();
+	console << endl << "Event details : " << e.getDetails();
+	return console;
+}
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //nominal tickets according to the desired characteristics
 //(eg: VIP, lawn, tribune, boxes, etc.)
-
-//class seatType
-//{
-//	string name = "";
-//	int totalRowNo = 0;
-//	int firstRow = 0;
-//
-//};
 
 
 //row number, seat number, price, event, const id
 //class Ticket
 //{
-//
+//	
 //};
 
 
@@ -198,10 +340,17 @@ class Event
 int main()
 {
 	//Date date1(29, February, 2001);
-	Date date1(25, February, 2001);
-	//Date date1(45, January, 2001);
+	//Date date1(15, February, 2001);
+	//Date date1(34, January, 2001);
+	//date1.printDate();
+	
 	//Time time1(14, 54);
 	//Time time1(78, 12);
-	
-	
+	/*Date date1(15, February, 2001);
+	Time time1(16, 15);*/
+	//Event event1("Concert Delia", "concert blablabla", date1, time1);
+	//cout << event1 << date1 << time1;
+	/*Time t1;
+	cin >> t1;
+	cout << t1;*/
 }
